022035fc4bd5b524a52076a09ba91bff56fa165c
spirit-124 <bajrangofficial580@gmail.com>
2023-03-02 19:58:34 +0530

    [Added]  Review  page

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactStars from "react-stars";
+import { reviewsRef } from "../firebase/firebase";
+import { addDoc } from "firebase/firestore";
+import { TailSpin } from "react-loader-spinner";
 
 const Review = () => {
-  return <div>Review</div>;
+  const [rating, setRating] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const sendReview = async () => {
+    try {
+      await addDoc(reviewsRef, {});
+      swal({
+        title: "review sent",
+        icon: "success",
+        button: false,
+        timer: 3000,
+      });
+    } catch (err) {
+      swal({
+        title: err.message,
+        icon: "error",
+        button: false,
+        timer: 3000,
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className=" mt-4 border-t-2 border-gray-600 w-full">
+        <ReactStars
+          className="mt-1"
+          count={5}
+          size={20}
+          onChange={(rate) => setRating(rate)}
+        />
+        <input
+          type="text"
+          placeholder="Share your thoughts...."
+          className="w-full header outline-none p-2 bg-slate-900 rounded-lg"
+        />
+        <button className="w-full p-2 bg-green-700 mt-2 flex justify-center ">
+          {loading ? <TailSpin height={25} color="white" /> : "Share"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Review;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -19,6 +19,7 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const moviesRef = collection(db, "movies");
+export const reviewsRef = collection(db, "reviews");
 
 
 export default app;
